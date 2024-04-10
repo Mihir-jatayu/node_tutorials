37b975b886d0e8a4a4c626c28bb5343a0af317a2
Mihir-jatayu <156747135+Mihir-jatayu@users.noreply.github.com>
2024-04-10 18:10:58 +0530

    Done

---
+++ api.js
@@ -0,0 +1,27 @@
+const data = [
+    {name:'mihir',email:'mihir@gamil.com'},
+    {name:'ankit',email:'ankit@gamil.com'},
+    {name:'rushiraj',email:'rushiraj@gamil.com'},
+];
+module.exports=data;
+
+
+
+// const colors = require('colors');
+// console.log('hello'.green); 
+// const http = require('http');
+// const data = require('./api');
+// http.createServer((req, res) => {
+    //     res.writeHead(200,{"Content-Type":'application\json'});
+//     res.write(JSON.stringify(data));
+//     res.end();
+// }).listen(2409);
+// app.get('',(req,res)=>{
+//     res.send(`<h1>Hello world</h1> <a href="/about"> go to about </a>`);
+// });
+
+// app.get('/about',(req,res)=>{
+//     res.send(`
+//      <input type="text" value="${(req.query.name)?req.query.name:''}"  placeholder="user Name " /> 
+//      `);
+// });
---
+++ index.js
@@ -0,0 +1,5 @@
+// const express = require('express');
+// const app =express();
+// console.log(__dirname)
+// app.listen(2409);
+
