956f3024e352fc913a1ec7935a31086bbfa99266
Mihirdave35 <mihirdave24@outlook.com>
2024-04-10 22:01:57 +0530

    pending ejs

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-// const express = require('express');
-// const app =express();
-// console.log(__dirname)
-// app.listen(2409);
+const express = require('express');
+const app =express();
+const path = require('path');
+const publicPath = path.join(__dirname,'public');
+// app.use(express.static(publicPath));
+app.set('view engine','ejs');
+
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get('/profile',(_,res)=>{
+    const user ={
+        name:'mihir',email:'mihirdave35@gmail.com'
+    }
+    res.render(`profile`,{user});
+})
+
+app.get('/about',(_,res)=>{
+    res.sendFile(`${publicPath}/about.html`);
+})
+
+
+app.get('/help',(_,res)=>{
+    res.sendFile(`${publicPath}/help.html`);
+})
+app.listen(2409);
 
